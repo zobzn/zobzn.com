3073fe1335dca79ce3d6ec8f41c816e933448d05
zema <zlobzn@gmail.com>
2019-12-25 14:51:40 +0200

    added jest

---
+++ src/lib/gtag.test.js
@@ -0,0 +1,13 @@
+import { pageview } from "./gtag";
+
+it("works", () => {
+  expect(window.dataLayer).toBeUndefined();
+  pageview("http://example.com/");
+  expect(window.dataLayer).toBeInstanceOf(Array);
+  expect(window.dataLayer).toHaveLength(1);
+  expect([...window.dataLayer[0]]).toEqual([
+    "config",
+    "UA-47099248-1",
+    { page_path: "http://example.com/" }
+  ]);
+});
